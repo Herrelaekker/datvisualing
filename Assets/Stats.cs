2cd750881c0d083f09b09dd3b92c55440fd32e54
Jacob <jacobhlnhtxb16@gmail.com>
2018-11-15 10:48:23 +0100

    lol

---
+++ Assets/Stats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    private int stat1;
+    private int stat2;
+
+	// Use this for initialization
+	void Start () {
+        stat1 = PlayerPrefs.GetInt("stat1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            stat1 += 1;
+            PlayerPrefs.SetInt("stat1", stat1);
+            Debug.Log(PlayerPrefs.GetInt("stat1"));
+        }
+	}
+}
